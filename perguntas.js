6a3104a591afff2bdc118123d8e1a5779590d36a
00gabriel15 <161468081+00gabriel15@users.noreply.github.com>
2024-10-22 09:38:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'time',
+    'maior idolo do flamengo? ',
+    'zico'
+)
+criaCartao(
+    'time',
+    'quantas libertadores tem o flamengo?',
+    '3'
+)
+criaCartao(
+    'time',
+    'estadio do flamengo?',
+    'maracana'
+)
+criaCartao(
+    'time',
+    'treinador do flamengo?',
+    'felipe luiz'
+)
+
